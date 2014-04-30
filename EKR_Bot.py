75e90fda41033b30d38cb410cb2c31ddf998c7ed
Michael Arnold <valestrum@hotmail.com>
2014-04-30 07:49:37 -0400

    Progress update.

--- EKR_Bot.py
+++ EKR_Bot.py
@@ -1,34 +1,41 @@
 # !! Note: !! This program currently does not have Reddit interaction enabled. This is only the web-scraped information half.
 
-
-
 # EVE: Online Killmail Reddit Bot (EKRB)
 # Created by: Reddit.com/u/Valestrum
 # Contribute to the open source project here: https://github.com/ArnoldM904/EK_Reddit_Bot
 
-import urllib # Access internet and make network requests
-import re  # Regex
+import urllib   # Access internet and make network requests
+import re       # Regex
+import praw     # Python Reddit API Wrapper
 
-killmail = raw_input("Enter zkillboard link: ")
-url = [killmail]
-i = 0
-iskDroppedSource = '<td class="item_dropped">(.+?)</td>' # Gets whatever is inbetween the tags
-iskDestroyedSource = '<td class="item_destroyed">(.+?)</td>'
-iskTotalSource = '<strong class="item_dropped">(.+?)</strong>'
+r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.0 - Created by /u/Valestrum '
+                                'https://github.com/ArnoldM904/EK_Reddit_Bot')
+r.login('InsertUsernameHere','InsertPasswordHere')
 
-iskDroppedText = re.compile(iskDroppedSource) # Converts above regex string into something that can be interpreted by regular library
-iskDestroyedText = re.compile(iskDestroyedSource)
-iskTotalText = re.compile(iskTotalSource)
+# -- Insert large work-in-progress code that I'm too ashamed to show here. -- 
 
-htmlfile = urllib.urlopen(url[i])
-htmltext = htmlfile.read()
+def readKillmail():
+        killmail = "https://zkillboard.com/kill/38412453/" # Change this to be user's killmail.
+        url = [killmail]
+        i = 0
+        iskDroppedSource = '<td class="item_dropped">(.+?)</td>' # Gets whatever is inbetween the tags
+        iskDestroyedSource = '<td class="item_destroyed">(.+?)</td>'
+        iskTotalSource = '<strong class="item_dropped">(.+?)</strong>'
 
-iskDropped = re.findall(iskDroppedText,htmltext)
-iskDestroyed = re.findall(iskDestroyedText,htmltext)
-iskTotal = re.findall(iskTotalText,htmltext)
+        iskDroppedText = re.compile(iskDroppedSource) # Converts above regex string into something that can be interpreted by regular library
+        iskDestroyedText = re.compile(iskDestroyedSource)
+        iskTotalText = re.compile(iskTotalSource)
 
-print("Hi, I am a killmail reader bot. Let me summarize this killmail for you!")
-print("Value dropped: " + str(iskDropped[0]))
-print("Value destroyed: " + str(iskDestroyed[0]))
-print("Total value: " + str(iskTotal[0]))
-print("^^This ^^bot ^^is ^^brand ^^new ^^please ^^be ^^gentle. ^^PM ^^for ^^questions.")
+        htmlfile = urllib.urlopen(url[i])
+        htmltext = htmlfile.read()
+
+        iskDropped = re.findall(iskDroppedText,htmltext)
+        iskDestroyed = re.findall(iskDestroyedText,htmltext)
+        iskTotal = re.findall(iskTotalText,htmltext)
+
+        print("Hi, I am a killmail reader bot. Let me summarize this killmail for you!")
+        print("Value dropped: " + str(iskDropped[0]))
+        print("Value destroyed: " + str(iskDestroyed[0]))
+        print("Total value: " + str(iskTotal[0]))
+        print("^^This ^^bot ^^is ^^brand ^^new ^^please ^^be ^^gentle. ^^PM ^^for ^^questions.")
+
