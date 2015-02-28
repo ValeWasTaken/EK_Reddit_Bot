30966686e279e0361b28ded3317cc718c1ff034f
Michael Arnold <valestrum@hotmail.com>
2015-02-27 20:39:15 -0500

    Added multi-killmail support & optimized scraping

--- EKR_Bot.py
+++ EKR_Bot.py
@@ -3,6 +3,7 @@
 import urllib   # Access internet and make network requests
 import re       # Regex
 import praw     # Python Reddit API Wrapper
+from bs4 import BeautifulSoup # Web scraping
 import time
 
 r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.1 - Created by /u/Valestrum '
@@ -14,46 +15,39 @@ def run_bot():
     with open('cache.txt','r') as cache:
         existing = cache.read().splitlines()
 
-    subreddit = r.get_subreddit("eve")
-    comments = subreddit.get_comments(limit=100)
+    subreddit = r.get_subreddit("test")
+    comments = subreddit.get_comments(limit=150)
 
     with open('cache.txt', 'a+') as cache:
         for comment in comments:
             comment_text = comment.body.lower()
 
             #Records any relevant URLs.
-            killmail = [item for item in comment_text.split() if re.match(r"https://zkillboard\.com/kill/*", item)]
+            killmails = [item for item in comment_text.split() if re.match(r"https://zkillboard\.com/kill/*", item)]
 
-            if killmail and comment.id not in existing: #if killmail list is not empty and bot has never messaged
-                killmail = killmail[0]
-                if killmail[:13] == 'https://zkill' or killmail[:12] == 'http://zkill':
-                    existing.append(comment.id)
-                    cache.write(comment.id + '\n')
-                    print("I found a new comment! The ID is: " + comment.id)
-                    report = read_killmail(killmail)
-                    comment.reply(report)
+            if killmails and comment.id not in existing: #if killmail list is not empty and bot has never messaged
+                mails = []
+                for mail in killmails:
+                    if mail[:13] == 'https://zkill' or mail[:12] == 'http://zkill':
+                        mails.append(str(mail))
+                existing.append(comment.id)
+                cache.write(comment.id + '\n')
+                print("I found a new comment! The ID is: " + comment.id)
+                report = read_killmail(mails)
+                comment.reply(report)
 
-def read_killmail(killmail):
-        url = [killmail]
-        i = 0
-        iskDroppedSource = '<td class="item_dropped">(.+?)</td>' # Gets whatever is inbetween the tags
-        iskDestroyedSource = '<td class="item_destroyed">(.+?)</td>'
-        iskTotalSource = '<strong class="item_dropped">(.+?)</strong>'
+def read_killmail(killmails):
+        replyData = []
+        for url in killmails:
+            soup = BeautifulSoup(urllib.urlopen(url).read())
+            iskDropped = soup.find("td", class_="item_dropped").get_text()
+            iskDestroyed = soup.find("td", class_="item_destroyed").get_text()
+            iskTotal = soup.find("strong", class_="item_dropped").get_text()
+            replyData.append("\n\n>Value dropped: " + str(iskDropped) +"\n\n>Value destroyed: " + str(iskDestroyed) +"\n\n>Total value: " + str(iskTotal)+'\n\n'+('-'*20))
+        replyData = ('\n\n'.join(replyData))
 
-        iskDroppedText = re.compile(iskDroppedSource) # Converts regex string into something that can be interpreted by regular library
-        iskDestroyedText = re.compile(iskDestroyedSource)
-        iskTotalText = re.compile(iskTotalSource)
-
-        html = urllib.urlopen(url[i]).read()
-
-        iskDropped = re.findall(iskDroppedText,html)
-        iskDestroyed = re.findall(iskDestroyedText,html)
-        iskTotal = re.findall(iskTotalText,html)
-
-        return("Hi, I am a killmail reader bot. Let me summarize this killmail for you!"
-        +"\n\n>Value dropped: " + str(iskDropped[0])
-        +"\n\n>Value destroyed: " + str(iskDestroyed[0])
-        +"\n\n>Total value: " + str(iskTotal[0])
+        return("Hi, I am a killmail reader bot. Let me summarize killmail for you!"
+        +str(replyData)
         +"\n\n^^This ^^bot ^^is ^^brand ^^new ^^please ^^be ^^gentle. ^^Please ^^PM ^^with ^^suggestions!")
 
 while True:
