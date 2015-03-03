d7fde31710b28e77cd5b29106edaa20726a07273
Michael Arnold <valestrum@hotmail.com>
2015-03-02 19:52:25 -0500

    Version 1.6 - Added link to victim's fitting

--- EKR_Bot.py
+++ EKR_Bot.py
@@ -6,7 +6,7 @@ import praw     # Python Reddit API Wrapper
 from bs4 import BeautifulSoup # Web scraping
 import time
 
-r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.5 - Created by /u/Valestrum '
+r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.6 - Created by /u/Valestrum '
                                 'Designed to help users get killmail info without clicking links.')
 r.login('UsernameHere','PasswordHere')
 loopCount = 0
@@ -47,6 +47,8 @@ def read_killmail(killmails):
             #v = victim, kb = pilot firing killing blow
             vPilotName = soup.table.table.tr.a.get('title')
             vShipType = (''.join(((soup.find("td", style="width: 100%").get_text())).split())) # Ex: Leviathan(Titan)
+            vRiggingText = soup.find_all('ul', class_="dropdown-menu")[3].find('a').get_text()
+            vRiggingLink = soup.find_all('ul', class_="dropdown-menu")[3].find_all('a', href=re.compile('/o.smium.org/loadout/'))[0]['href']
             #Add v corp and alliance here
             kbShipType = soup.find_all('tr', class_="attacker")[0].find_all('a', href=re.compile('/ship/'))[0].img.get('alt') #Ex: Nyx
             kbPilotName = soup.find_all('td', style="text-align: center;")[0].find_all('a', href=re.compile('/character/'))[0].img.get('alt')
@@ -55,7 +57,7 @@ def read_killmail(killmails):
             otherPilots = int(str(soup.find("th", class_="hidden-md hidden-xs").get_text())[:-9])-1 #Ex: '44' out of "45 Involved", excluded 1 being kb
             
             replyData.append("\n\n>A %s piloted by %s was destroyed in system %s by %s flying a %s along with %s others." % (vShipType,vPilotName,system,kbPilotName,kbShipType,otherPilots)
-                    +"\n\n>Value dropped: %s\n\n>Value destroyed: %s\n\n>Total value: %s\n\n" % (iskDropped,iskDestroyed,iskTotal)+('-'*50))
+                    +"\n\n>Value dropped: %s\n\n>Value destroyed: %s\n\n>Total value: %s\n\n>[%s](%s)\n\n" % (iskDropped,iskDestroyed,iskTotal,vRiggingText,vRiggingLink)+('-'*50))
         replyData = ('\n\n'.join(replyData))
 
         return("Hi, I am a killmail reader bot. Let me summarize killmail for you!"
