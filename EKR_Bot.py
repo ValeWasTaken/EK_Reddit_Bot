e4ce76866aa2c81c42968ed228af11d3b7c1eb1b
Michael Arnold <valestrum@hotmail.com>
2015-03-06 13:42:46 -0500

    Updated to version 1.92
    - Added grammar fix "a" to "an" when needed on ships
    - Added contribution links
    - Added "others" to "other" fix when needed on involved pilots

--- EKR_Bot.py
+++ EKR_Bot.py
@@ -6,7 +6,7 @@ import praw     # Python Reddit API Wrapper
 from bs4 import BeautifulSoup # Web scraping
 import time
 
-r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.9 - Created by /u/Valestrum '
+r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.92 - Created by /u/Valestrum '
                                 'Designed to help users get killmail info without clicking links.')
 r.login('UsernameHere','PasswordHere')
 loopCount = 0
@@ -43,7 +43,7 @@ def read_killmail(killmails):
             iskDropped = soup.find("td", class_="item_dropped").get_text()
             iskDestroyed = soup.find("td", class_="item_destroyed").get_text()
             iskTotal = soup.find("strong", class_="item_dropped").get_text()
-            
+
             dropRounded = (int((iskDropped)[:-7].replace(',','')) / 1000000.0)
             destroyedRounded = (int((iskDestroyed)[:-7].replace(',','')) / 1000000.0)
             totalRounded = (int((iskTotal)[:-7].replace(',','')) / 1000000.0)
@@ -82,34 +82,46 @@ def read_killmail(killmails):
             else:
                     vCorp = '<No Corp>'
                     vAlliance = '<No Alliance>'
+                    
             vShipType = (''.join(((soup.find("td", style="width: 100%").get_text())).split())) # Ex: Leviathan(Titan)
+            if vShipType[0].lower() in 'aeiou':
+                    vShipType = 'n '+str(vShipType)
+            else:
+                    vShipType = ' '+str(vShipType)
             vRiggingText = soup.find_all('ul', class_="dropdown-menu")[3].find('a').get_text()
             vRiggingLink = soup.find_all('ul', class_="dropdown-menu")[3].find_all('a', href=re.compile('/o.smium.org/loadout/'))[0]['href']
             
             kbShipType = soup.find_all('tr', class_="attacker")[0].find_all('a', href=re.compile('/ship/'))[0].img.get('alt') #Ex: Nyx
+            if kbShipType[0].lower() in 'aeiou':
+                    kbShipType = 'n '+str(kbShipType)
+            else:
+                    kbShipType = ' '+str(kbShipType)
             if int(otherPilots) == 0:
                     kbPilotInfo = soup.find('div', class_="hidden-sm hidden-md hidden-xs").get_text().split('\n\n')
                     kbPilotName = kbPilotInfo[0]
                     if len(kbPilotInfo) > 1:
                             kbCorp = kbPilotInfo[1]
                             if len(kbPilotInfo) > 3:
-                                    kbAlliance = kbPilotInfo[2][:-1] # For some reason an extra space kept being added at the end.
+                                    kbAlliance = kbPilotInfo[2]
                             else:
                                     kbAlliance = '<No Alliance>'
                     else:
                             kbCorp = '<No Corp>'
                             kbAlliance = '<No Alliance>'
-                    replyData.append("\n\n>On %s a %s piloted by %s of (%s | %s) was destroyed in system %s by %s of (%s | %s) flying a %s along with %s others." % (date,vShipType,vPilotName,vCorp,vAlliance,system,kbPilotName,kbCorp,kbAlliance,kbShipType,otherPilots)
+                    replyData.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s of (%s | %s) flying a%s along with %s others." % (date,vShipType,vPilotName,vCorp,vAlliance,system,kbPilotName,kbCorp,kbAlliance,kbShipType,otherPilots)
                     +"\n\n>Value dropped: %s\n\n>Value destroyed: %s\n\n>Total value: %s\n\n>[%s's %s](%s)\n\n" % (iskDropped,iskDestroyed,iskTotal,vPilotName,vRiggingText,vRiggingLink)+('-'*50))
             else:
                     kbPilotName = soup.find_all('td', style="text-align: center;")[0].find_all('a', href=re.compile('/character/'))[0].img.get('alt')
-                    replyData.append("\n\n>On %s a %s piloted by %s of (%s | %s) was destroyed in system %s by %s flying a %s along with %s others." % (date,vShipType,vPilotName,vCorp,vAlliance,system,kbPilotName,kbShipType,otherPilots)
+                    if int(otherPilots) == 1:
+                            replyData.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s flying a%s along with %s other." % (date,vShipType,vPilotName,vCorp,vAlliance,system,kbPilotName,kbShipType,otherPilots))
+                    else:
+                            replyData.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s flying a%s along with %s others." % (date,vShipType,vPilotName,vCorp,vAlliance,system,kbPilotName,kbShipType,otherPilots)
                     +"\n\n>Value dropped: %s\n\n>Value destroyed: %s\n\n>Total value: %s\n\n>[%s's %s](%s)\n\n" % (iskDropped,iskDestroyed,iskTotal,vPilotName,vRiggingText,vRiggingLink)+('-'*50))
         replyData = ('\n\n'.join(replyData))
 
         return("Hi, I am a killmail reader bot. Let me summarize killmail for you!"
         +str(replyData)
-        +"\n\n^^This ^^bot ^^is ^^open ^^source ^^and ^^in ^^development! ^^Please ^^feel ^^free ^^to ^^contribute ^^[here](https://github.com/ArnoldM904/EK_Reddit_Bot) ^^and/or ^^PM ^^with ^^suggestions!")
+        +"\n\n^^This ^^bot ^^is ^^open ^^source ^^& ^^in ^^active ^^development! ^^Please ^^feel ^^free ^^to ^^contribute: ^^[Suggestions](%s) ^^| ^^[Code](%s) ^^| ^^[ISK](%s) ^^| ^^[Love](%s)") % ('http://www.reddit.com/message/compose?to=Killmail_Bot','https://github.com/ArnoldM904/EK_Reddit_Bot/blob/master/EKR_Bot.py','http://evewho.com/pilot/Valestrum+Vos','http://www.reddit.com/message/compose?to=Killmail_Bot')
 
 while True:
     run_bot()
