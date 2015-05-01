edf8c4725cf1954ce6ac10fbac1550918229fc96
Michael Arnold <valestrum@hotmail.com>
2015-04-30 23:39:01 -0400

    Updating version # forgotten in last update

--- EKR_Bot.py
+++ EKR_Bot.py
@@ -6,7 +6,7 @@ import praw     # Python Reddit API Wrapper
 from bs4 import BeautifulSoup # Web scraping
 import time
 
-r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.941 - Created by /u/Valestrum '
+r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.942 - Created by /u/Valestrum '
                                 'Designed to help users get killmail info without clicking links.')
 r.login('UsernameHere','PasswordHere')
 loopCount = 0
