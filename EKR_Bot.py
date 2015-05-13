dba5a4d2a5b6f680676c8521d53d26fae3adc247
Michael Arnold <valestrum@hotmail.com>
2015-05-13 17:45:02 -0400

    Replaced CamelCase for conversion into PEP-8

--- EKR_Bot.py
+++ EKR_Bot.py
@@ -10,7 +10,7 @@ import requests # Allows for catching ConnectionErrors and rerunning the program
 r = praw.Reddit(user_agent='EVE: Online Killmail Reader Bot v1.944 - Created by /u/Valestrum '
                                 'Designed to help users get killmail info without clicking links.')
 r.login('UsernameHere','PasswordHere')
-loopCount = 0
+loop_count = 0
 
 def condense_value(num, suffix='ISK'):
     if num > 999999999999999:
@@ -50,68 +50,67 @@ def read_killmail(killmails):
         replyData = []
         for url in killmails:
             soup = BeautifulSoup(urllib.urlopen(url).read())
-            iskDropped = soup.find("td", class_="item_dropped").get_text()
-            iskDestroyed = soup.find("td", class_="item_destroyed").get_text()
-            iskTotal = soup.find("strong", class_="item_dropped").get_text()
-            iskDropped, iskDestroyed, iskTotal = [condense_value(int(value[:-7].replace(',',''))) for value in [iskDropped, iskDestroyed, iskTotal]]
+            isk_dropped = soup.find("td", class_="item_dropped").get_text()
+            isk_destroyed = soup.find("td", class_="item_destroyed").get_text()
+            isk_total = soup.find("strong", class_="item_dropped").get_text()
+            isk_dropped, isk_destroyed, isk_total = [condense_value(int(value[:-7].replace(',',''))) for value in [isk_dropped, isk_destroyed, isk_total]]
 
             system = soup.find_all('a', href=re.compile('/system/'))[1].get_text() #Ex: Iralaja
             date = soup.find("table", class_="table table-condensed table-striped table-hover").find_all('td')[3].get_text()[:10]
             if len(date) < 6:
                     date = soup.find("table", class_="table table-condensed table-striped table-hover").find_all('td')[2].get_text()[:10]
-            otherPilots = int(str(soup.find("th", class_="hidden-md hidden-xs").get_text())[:-9])-1 #Ex: '44' out of "45 Involved", excluded 1 being kb
+            other_pilots = int(str(soup.find("th", class_="hidden-md hidden-xs").get_text())[:-9])-1 #Ex: '44' out of "45 Involved", excluded 1 being kb
             
             #v = victim, kb = pilot firing killing blow
-            vPilotInfo = soup.find("table", class_="table table-condensed").find_all('td')[2].get_text().split('\n\n')
-            vPilotName = vPilotInfo[0]
-            if len(vPilotInfo) > 1:
-                    vCorp = vPilotInfo[1]
-                    if len(vPilotInfo) > 3: # This accounts for extra variable '' added to PilotInfo
-                            vAlliance = vPilotInfo[2]
+            v_pilot_info = soup.find("table", class_="table table-condensed").find_all('td')[2].get_text().split('\n\n')
+            v_pilot_name = v_pilot_info[0]
+            if len(v_pilot_info) > 1:
+                    v_corp = v_pilot_info[1]
+                    if len(v_pilot_info) > 3: # This accounts for extra variable '' added to v_pilot_info
+                            v_alliance = v_pilot_info[2]
                     else:
-                            vAlliance = '<No Alliance>'
+                            v_alliance = '<No Alliance>'
             else:
-                    vCorp = '<No Corp>'
-                    vAlliance = '<No Alliance>'
+                    v_corp = '<No Corp>'
+                    v_alliance = '<No Alliance>'
                     
-            vShipType = (''.join(((soup.find("td", style="width: 100%").get_text())).split())) # Ex: Leviathan(Titan)
-            if vShipType[0].lower() in 'aeiou':
-                    vShipType = 'n '+str(vShipType)
+            v_ship_type = (''.join(((soup.find("td", style="width: 100%").get_text())).split())) # Ex: Leviathan(Titan)
+            if v_ship_type[0].lower() in 'aeiou':
+                    v_ship_type = 'n '+str(v_ship_type)
             else:
-                    vShipType = ' '+str(vShipType)
-            vRiggingText = soup.find_all('ul', class_="dropdown-menu")[3].find('a').get_text()
-            vRiggingLink = soup.find_all('ul', class_="dropdown-menu")[3].find_all('a', href=re.compile('/o.smium.org/loadout/'))[0]['href']
+                    v_ship_type = ' '+str(v_ship_type)
+            v_rigging_text = soup.find_all('ul', class_="dropdown-menu")[3].find('a').get_text()
+            v_rigging_link = soup.find_all('ul', class_="dropdown-menu")[3].find_all('a', href=re.compile('/o.smium.org/loadout/'))[0]['href']
             
-            kbShipType = soup.find_all('tr', class_="attacker")[0].find_all('a', href=re.compile('/ship/'))[0].img.get('alt') #Ex: Nyx
-            if kbShipType[0].lower() in 'aeiou':
-                    kbShipType = 'n '+str(kbShipType)
+            kb_ship_type = soup.find_all('tr', class_="attacker")[0].find_all('a', href=re.compile('/ship/'))[0].img.get('alt') #Ex: Nyx
+            if kb_ship_type[0].lower() in 'aeiou':
+                    kb_ship_type = 'n '+str(kb_ship_type)
             else:
-                    kbShipType = ' '+str(kbShipType)
-            if int(otherPilots) == 0:
-                    kbPilotInfo = soup.find('div', class_="hidden-sm hidden-md hidden-xs").get_text().split('\n\n')
-                    kbPilotName = kbPilotInfo[0]
-                    if len(kbPilotInfo) > 1:
-                            kbCorp = kbPilotInfo[1]
-                            if len(kbPilotInfo) > 3:
-                                    kbAlliance = kbPilotInfo[2]
+                    kb_ship_type = ' '+str(kb_ship_type)
+            if int(other_pilots) == 0:
+                    kb_pilot_info = soup.find('div', class_="hidden-sm hidden-md hidden-xs").get_text().split('\n\n')
+                    kb_pilot_name = kb_pilot_info[0]
+                    if len(kb_pilot_info) > 1:
+                            kb_corp = kb_pilot_info[1]
+                            if len(kb_pilot_info) > 3:
+                                    kb_alliance = kb_pilot_info[2]
                             else:
-                                    kbAlliance = '<No Alliance>'
+                                    kb_alliance = '<No Alliance>'
                     else:
-                            kbCorp = '<No Corp>'
-                            kbAlliance = '<No Alliance>'
-                    replyData.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s of (%s | %s) flying a%s along with %s others." % (date,vShipType,vPilotName,vCorp,vAlliance,system,kbPilotName,kbCorp,kbAlliance,kbShipType,otherPilots))
-                
+                            kb_corp = '<No Corp>'
+                            kb_alliance = '<No Alliance>'
+                    reply_data.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s of (%s | %s) flying a%s along with %s others." % (date,v_ship_type,v_pilot_name,v_corp,v_alliance,system,kb_pilot_name,kb_corp,kb_alliance,kb_ship_type,other_pilots))
             else:
-                    kbPilotName = soup.find_all('td', style="text-align: center;")[0].find_all('a', href=re.compile('/character/'))[0].img.get('alt')
-                    if int(otherPilots) == 1:
-                            replyData.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s flying a%s along with %s other." % (date,vShipType,vPilotName,vCorp,vAlliance,system,kbPilotName,kbShipType,otherPilots))
+                    kb_pilot_name = soup.find_all('td', style="text-align: center;")[0].find_all('a', href=re.compile('/character/'))[0].img.get('alt')
+                    if int(other_pilots) == 1:
+                            reply_data.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s flying a%s along with %s other." % (date,v_ship_type,v_pilot_name,v_corp,v_alliance,system,kb_pilot_name,kb_ship_type,other_pilots))
                     else:
-                            replyData.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s flying a%s along with %s others." % (date,vShipType,vPilotName,vCorp,vAlliance,system,kbPilotName,kbShipType,otherPilots))
-            replyData.append("\n\n>Value dropped: %s\n\n>Value destroyed: %s\n\n>Total value: %s\n\n>[%s's %s](%s)\n\n" % (iskDropped,iskDestroyed,iskTotal,vPilotName,vRiggingText,vRiggingLink)+('-'*50))
-        replyData = ('\n\n'.join(replyData))
+                            reply_data.append("\n\n>On %s a%s piloted by %s of (%s | %s) was destroyed in system %s by %s flying a%s along with %s others." % (date,v_ship_type,v_pilot_name,v_corp,v_alliance,system,kb_pilot_name,kb_ship_type,other_pilots))
+            reply_data.append("\n\n>Value dropped: %s\n\n>Value destroyed: %s\n\n>Total value: %s\n\n>[%s's %s](%s)\n\n" % (isk_dropped,isk_destroyed,isk_total,v_pilot_name,v_rigging_text,v_rigging_link)+('-'*50))
+        reply_data = ('\n\n'.join(reply_data))
 
         return("Hi, I am a killmail reader bot. Let me summarize killmail for you!"
-        +str(replyData)
+        +str(reply_data)
         +"\n\n^^This ^^bot ^^is ^^open ^^source ^^& ^^in ^^active ^^development! ^^Please ^^feel ^^free ^^to ^^contribute: ^^[Suggestions](%s) ^^| ^^[Code](%s)") % ('http://www.reddit.com/message/compose?to=Killmail_Bot','https://github.com/ArnoldM904/EK_Reddit_Bot/blob/master/EKR_Bot.py')
 
 while True:
@@ -121,6 +120,6 @@ while True:
         print e
         time.sleep(60)
         run_bot()
-    loopCount += 1
-    print("Program loop #"+str(loopCount)+" completed successfully.")
+    loop_count += 1
+    print("Program loop #"+str(loop_count)+" completed successfully.")
     time.sleep(1200)
